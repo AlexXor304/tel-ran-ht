09c8365e00ae42ff05a754d1876b875eaa2b2e13
Alexandr <akhor304@live.com>
2017-12-07 14:29:39 +0200

    First commit

--- 41-InputOutput_HT_07_12_2017/src/Main.java
+++ 41-InputOutput_HT_07_12_2017/src/Main.java
@@ -1,7 +1,10 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    private static final AtomicInteger integer = new AtomicInteger(0);
+
     public static void main(String[] args) {
 
         Thread thread = new Thread(new Runnable() {
@@ -10,35 +13,31 @@ public class Main {
 
             @Override
             public void run() {
-                int i = 0;
-                while(true){
+                while (true) {
                     s = scanner.nextLine();
-                    i++;
 
-                    if(s.equals("-1")) break;
-                    try {
-                        writeFile(s + "\n", Integer.toString(i) );
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    if (s.equals("-1")) break;
+                    if (!s.equals("")) {
+                        integer.getAndIncrement();
+                        try {
+                            writeFile(s + "\n", Integer.toString(Integer.parseInt(integer.toString())));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }
             }
         });
-    thread.run();
+        thread.run();
 
 
     }
 
-    private static void createFile() throws FileNotFoundException {
-        File file = new File("/users/asx/Documents/files/file.txt");
-        InputStream inputStream = new FileInputStream(file);
-        OutputStream outputStream = System.out;
-    }
 
-    private static void writeFile(String s, String name) throws IOException{
+    private static void writeFile(String s, String name) throws IOException {
 
-        FileWriter fileWriter = new FileWriter("/users/asx/Documents/files/file" + name +".txt", true);
+        FileWriter fileWriter = new FileWriter("/users/asx/Documents/files/file" + name + ".txt", false);
         fileWriter.write(s);
         fileWriter.flush();
 
