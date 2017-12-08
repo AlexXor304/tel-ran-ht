6faec743ce0b0e631a23ff2519177b25e418196d
Alexandr <akhor304@live.com>
2017-12-08 10:34:49 +0200

    Level 3

--- 41-InputOutput_HT_07_12_2017/src/Main.java
+++ 41-InputOutput_HT_07_12_2017/src/Main.java
@@ -1,44 +1,20 @@
-import java.io.*;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class Main {
-    private static final AtomicInteger integer = new AtomicInteger(0);
 
     public static void main(String[] args) {
 
-        Thread thread = new Thread(new Runnable() {
-            Scanner scanner = new Scanner(System.in);
-            String s;
+        Scanner scanner = new Scanner(System.in);
+        String s;
 
-            @Override
-            public void run() {
-                while (true) {
-                    s = scanner.nextLine();
-
-                    if (s.equals("-1")) break;
-                    if (!s.equals("")) {
-                        integer.getAndIncrement();
-                        try {
-                            writeFile(s + "\n", Integer.toString(Integer.parseInt(integer.toString())));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                }
+        while (true) {
+            s = scanner.nextLine();
+            if (s.equals("-1")) break;
+            if (!s.equals("")) {
+                new FileThread(s).start();
             }
-        });
-        thread.start();
-
-
-    }
-
-    private static void writeFile(String s, String name) throws IOException {
-
-        FileWriter fileWriter = new FileWriter("/users/asx/Documents/files/file" + name + ".txt", false);
-        fileWriter.write(s);
-        fileWriter.flush();
+        }
 
     }
 }
