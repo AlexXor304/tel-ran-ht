a8cd372b8530b46a89e008a225c8388554af5aea
Alexandr <akhor304@live.com>
2017-12-08 10:39:46 +0200

    Level 3

--- 41-InputOutput_HT_07_12_2017/src/Main.java
+++ 41-InputOutput_HT_07_12_2017/src/Main.java
@@ -1,4 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Main {
@@ -17,4 +20,47 @@ public class Main {
         }
 
     }
+}
+
+class FileThread extends Thread {
+
+    String text;
+
+    public FileThread(String text) {
+        this.text = text;
+    }
+
+    private static final AtomicInteger integer = new AtomicInteger(0);
+
+    @Override
+    public void run() {
+
+        integer.getAndIncrement();
+        writeFile(text + "\n", Integer.toString(Integer.parseInt(integer.toString())));
+        System.out.println(Thread.currentThread().getName());
+
+    }
+
+    private void writeFile(String text, String name) {
+
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter("/users/asx/Documents/files/file" + name + ".txt", false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fileWriter.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
